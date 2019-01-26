08191e91c83af211a54ef575818f787da5df0a9d
JTK222 <lukasporwit3@googlemail.com>
2019-01-26 19:10:29 +0100

    Added usage instructions on Clock Core

--- src/main/java/net/dark_roleplay/medieval/common/objects/blocks/old/ClockDial.java
+++ src/main/java/net/dark_roleplay/medieval/common/objects/blocks/old/ClockDial.java
@@ -3,7 +3,10 @@ package net.dark_roleplay.medieval.common.objects.blocks.old;
 
 import static net.dark_roleplay.medieval.common.objects.blocks.BlockProperties.FACING_HORIZONTAL;
 
+import java.util.List;
 import java.util.Random;
+
+import javax.annotation.Nullable;
 
 import net.dark_roleplay.medieval.common.handler.MedievalBlocksOld;
 import net.minecraft.block.Block;
@@ -14,6 +17,8 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -25,6 +30,8 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ClockDial extends Block{
 
@@ -253,6 +260,12 @@ public class ClockDial extends Block{
 		}
 		return false;
 	}
+
+    @Override
+	@SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag){
+    	tooltip.add(I18n.format("drpmedieval.item.clock_dial.tooltip", I18n.format("tile.drpmedieval.clock_core.name")));
+    }
 
 	private AxisAlignedBB rotateAABB(AxisAlignedBB bb, int amount){
 		switch(amount){
